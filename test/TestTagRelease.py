da50f001c6d95c25577ddc9812fa2cf836322878
Sebastien Besson <seb.besson@gmail.com>
2013-08-08 13:39:49 +0100

    Add integration test for testing the new tag-release command

---
+++ test/TestTagRelease.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2012-2013 University of Dundee & Open Microscopy Environment
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import os
+import uuid
+import shutil
+import unittest
+import tempfile
+
+from scc import *
+from Sandbox import *
+from subprocess import Popen
+
+
+class TestTagRelease(SandboxTest):
+
+    def setUp(self):
+
+        super(TestTagRelease, self).setUp()
+
+        import random
+        self.new_tag = '%s.%s.%s' % (random.randint(2,100),
+            random.randint(0,100), random.randint(0,100))
+
+    def get_tags(self):
+        p = Popen(["git","tag"],stdout=subprocess.PIPE).communicate()[0]
+        return p.split("\n")
+
+    def testTag(self):
+
+        if self.new_tag in  self.get_tags():
+            pass
+
+        # Tag a new release
+        main(["tag-release", "--no-ask", self.new_tag])
+        self.assertTrue('v.' + self.new_tag in self.get_tags())
+
+if __name__ == '__main__':
+    unittest.main()
