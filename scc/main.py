92f7bceed8f5a6a2329969d4dad7e0cd43ed02fd
Sebastien Besson <seb.besson@gmail.com>
2013-11-06 11:49:01 +0000

    Properly handle SystemExit exceptions

--- scc/main.py
+++ scc/main.py
@@ -61,6 +61,8 @@ def entry_point():
     except Stop, stop:
         print stop,
         sys.exit(stop.rc)
+    except SystemExit, s:
+        raise
     except:
         traceback.print_exc()
         sys.exit(1)
