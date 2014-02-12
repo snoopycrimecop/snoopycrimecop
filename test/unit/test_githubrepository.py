7c3b3074b261f96cd0888539786944c90cd498b9
Sebastien Besson <seb.besson@gmail.com>
2014-02-12 11:17:37 +0000

    Factorize setup_org() and setup_repo() in GH repo unit tests

--- test/unit/test_githubrepository.py
+++ test/unit/test_githubrepository.py
@@ -49,7 +49,6 @@ class TestGithubRepository(MoxTestBase):
         self.repo.organization = None
 
         self.pulls = []
-
         self.gh.get_repo(
             "%s/%s" % (self.user.login, self.repo.name)).AndReturn(self.repo)
 
@@ -66,14 +65,20 @@ class TestGithubRepository(MoxTestBase):
         for x in self.pulls:
             yield x
 
-    @pytest.mark.parametrize('with_org', [True, False])
-    def test_init(self, with_org):
-        if with_org:
-            self.repo.organization = self.org
-            self.gh.get_organization(self.org.login).AndReturn(self.org)
+    def setup_org(self):
+        self.repo.organization = self.org
+        self.gh.get_organization(self.org.login).AndReturn(self.org)
+
+    def setup_repo(self):
         self.mox.ReplayAll()
         self.gh_repo = GitHubRepository(
             self.gh, self.user.login, self.repo.name)
+
+    @pytest.mark.parametrize('with_org', [True, False])
+    def test_init(self, with_org):
+        if with_org:
+            self.setup_org()
+        self.setup_repo()
         assert self.gh_repo.gh == self.gh
         assert self.gh_repo.repo == self.repo
         assert self.gh_repo.user_name == self.user.login
@@ -84,34 +89,26 @@ class TestGithubRepository(MoxTestBase):
             assert self.gh_repo.org is None
 
     def test_repr(self):
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         repo_str = "Repository: %s/%s" % (self.user.login, self.repo.name)
         assert str(self.gh_repo) == repo_str
 
     def test_get_issue(self):
         issue = self.mox.CreateMock(Issue)
         self.repo.get_issue(1).AndReturn(issue)
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         assert self.gh_repo.get_issue(1) == issue
 
     def test_get_pull(self):
         pullrequest = self.mox.CreateMock(PullRequest)
         self.repo.get_pull(1).AndReturn(pullrequest)
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         assert self.gh_repo.get_pull(1) == pullrequest
 
     def test_get_pulls(self):
         pulls = self.mox.CreateMock(PaginatedList)
         self.repo.get_pulls().AndReturn(pulls)
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         assert self.gh_repo.get_pulls() == pulls
 
     def test_get_pulls_by_base(self):
@@ -119,16 +116,12 @@ class TestGithubRepository(MoxTestBase):
         pulls_list = self.mox.CreateMock(PaginatedList)
         pulls_list.__iter__().AndReturn(self.iter_pulls())
         self.repo.get_pulls().AndReturn(pulls_list)
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         assert self.gh_repo.get_pulls_by_base("master") == \
             self.pulls[:-1]
 
     def test_get_owner(self):
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         assert self.gh_repo.get_owner() == self.user.login
 
     def test_create_open_pr(self):
@@ -139,8 +132,6 @@ class TestGithubRepository(MoxTestBase):
         head = "mock-head"
         self.repo.create_pull(
             title, description, base, head).AndReturn(pullrequest)
-        self.mox.ReplayAll()
-        self.gh_repo = GitHubRepository(
-            self.gh, self.user.login, self.repo.name)
+        self.setup_repo()
         assert self.gh_repo.create_pull(title, description, base, head) == \
             pullrequest
