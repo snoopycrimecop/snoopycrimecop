c8f751ab637b2a16328f223f46500b0d0f9a3564
Sebastien Besson <seb.besson@gmail.com>
2014-02-27 14:19:33 +0000

    Fix filter key/value pair parsing to allow leading hash symbols

--- scc/git.py
+++ scc/git.py
@@ -1703,7 +1703,7 @@ ALL sets user:#all as the default include filter. Default: ORG.""")
 
     def _parse_key_value(self, ftype, key_value):
         """Parse a key/value pattern of type key/value"""
-        pattern = re.compile(r'^(?P<key>(\w+)(/\w+)?):(?P<value>(\w+))$')
+        pattern = re.compile(r'^(?P<key>(\w+)(/\w+)?):(?P<value>#?(\w+))$')
         m = pattern.match(key_value)
         if not m:
             return False
